33d240746fbea3796202288e3490021dced9e441
Eti Daniel <danieleti@gmail.com>
2021-10-01 14:02:57 -0400

    adding queries group by

---
+++ 3_queries_group_by/5_avg_completion_time.sql
@@ -0,0 +1,6 @@
+SELECT students.name as name, AVG(assignment_submissions.duration) as average
+FROM students JOIN assignment_submissions
+ON students.id = assignment_submissions.student_id
+WHERE students.end_date IS NULL
+GROUP BY name
+ORDER BY average DESC;
